c0521297d573289ff5f126ef6c425a37ae9749a3
Syokii <35359184+Syokii@users.noreply.github.com>
2018-04-04 19:53:30 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,13 +7,6 @@ bot.on("ready", function() {
     console.log("Ready");
 });
 
-bot.on("guildMemberAdd", function(member) {
-    member.guild.channels.find("name", "general").sendMessage(`**Welcome ${member} to Nemesis Gaming. Enjoy the server!**`);
-
-    member.addRole(member.guild.roles.find("name", "Friends"));
-});
-
-//HELP
 bot.on("message", async message => {
     if(message.author.bot) return;
     if(message.channel.type === "dm") return;
@@ -25,52 +18,21 @@ bot.on("message", async message => {
     if(!command.startsWith(prefix)) return;
     
     if(command === `${prefix}help`) {        
-        var embedhelpmember = new Discord.RichEmbed()
-        .setTitle("**List of Help Commands**\n")
-        .addField("help.fun", "Displays fun commands from the bot.") 
-        .addField("help.info", "Displays account information commands.") 
-        .addField("help.admin", "Displays commands that can only be used by ADMINS.");
-        message.channel.sendEmbed(embedhelpmember);
-    }
-    
-    if(command === `${prefix}help.fun`) {        
-        var embedhelpfun = new Discord.RichEmbed()
-        .setTitle("**List of Fun Commands**")
-        .setColor("#02e3fc")
-        .addField("Coinflip", "Flips a coin randomly.") 
-        .addField("Roll", "Rolls a dice and you get a random number.") 
-        .addField("8ball", "Answers to all of your questions! (Correct usage: !8ball <question>)");
-        message.channel.sendEmbed(embedhelpfun);
-    }
-    
-    if(command === `${prefix}help.info`) {     
-        var embedhelpadmin = new Discord.RichEmbed()
-        .setTitle("**List of Information Commands**\n")
-        .setColor("#02e3fc")
-        .addField("Ping", "Tests your ping. (Correct usage: !ping)")
-        .addField("Avatar", "Shows the mentioned user's profile picture.") 
-        .addField("Userinfo", "Shows information about yourself.");
-        message.channel.sendEmbed(embedhelpadmin);
-    }
-    
-    if(command === `${prefix}help.admin`) {     
-        var embedhelpadmin = new Discord.RichEmbed()
-        .setTitle("**List of Admin Commands**\n")
-        .addField("Kick", "Kicks the mentioned user.") 
-        .addField("Ban", "Bans the mentioned user.") 
-        .addField("Admin", "Gives the mentioned user admin role. Must have Admin or higher role to give.") 
-        .addField("Mod", "Gives the mentioned user Mod role. Must have Admin or higher role to give.") 
-        .addField("Trusted", "Gives the mentioned user Trusted role. Must have Admin or higher role to give.") 
-        .addField("Mute", "The mentioned user will not be able to talk in chat.")
-        .addField("Unmute", "The muted user wil be able to talk in chat.")
-        .setColor("#02e3fc")
-        message.channel.sendEmbed(embedhelpadmin);
+        var embedhelp = new Discord.RichEmbed()
+        .setTitle("**List of Commands**\n")
+        .addField("Coinflip", "Flips a coin.") 
+        .addField("Roll", "Rolls a dice.") 
+        .addField("Ping", "Shows your ping status.")
+        .addField("Roll", "Rolls a dice.") 
+        .addField("8ball", "Answers all questions.") 
+        .addField("Avatar", "Displays the user profile picture.") 
+        .addField("Userinfo", "Shows information about your account.") 
+        .addField("Ban", "Bans the user provideded.")
+        .addField("Kick", "Kicks the user provided.")
+        message.channel.sendEmbed(embedhelp);
     }
 });
 
-//Music Commands
-
-//Fun Commands
 bot.on("message", async message => {
     if(message.author.bot) return;
     if(message.channel.type === "dm") return;
@@ -87,7 +49,7 @@ bot.on("message", async message => {
     }
 
     if(command === `${prefix}roll`) {
-        var roll = Math.floor(Math.random() * 100) + 1;
+        var roll = Math.floor(Math.random() * 6) + 1;
         message.channel.send("You rolled a " + roll);
     }
 
@@ -127,16 +89,11 @@ bot.on("message", async message => {
         message.channel.sendEmbed(embed);
     }
 
-    if(command === `${prefix}invite`) {
-        message.reply("https://discord.gg/RtStjyN");
-    }
-
     if(command === `${prefix}ping`) {
         message.channel.sendMessage('Pong! Your ping is `' + `${Date.now() - message.createdTimestamp}` + ' ms`');
     }
 });
 
-//Admin Commands
 bot.on("message", async message => {
     if(message.author.bot) return;
     if(message.channel.type === "dm") return;
@@ -168,56 +125,8 @@ bot.on("message", async message => {
         message.reply("The user has been kicked from this server. Keep this server awesome!");
         member.guild.channels.find("name", "staff-log").sendMessage(`${member} **has been kicked by** ${message.author.tag}`);        
     }
-
-    if(command === `${prefix}admin`) {
-        if (!message.member.permissions.has('ADMINISTRATOR')) return message.reply("Sorry, you don't have permissions to use this!");
-        const member = message.mentions.members.first();
-        if (!member) return message.reply('Invalid usage, please mention the person in this server!');
-        member.addRole(message.guild.roles.find("name", "Admin"));
-        message.reply("That user now has Admin role!");
-        member.guild.channels.find("name", "staff-log").sendMessage(`${member} **has been given admin by** ${message.author.tag}`);        
-    }
-
-    if(command === `${prefix}mod`) {
-        if (!message.member.permissions.has('ADMINISTRATOR')) return message.reply("Sorry, you don't have permissions to use this!");
-        const member = message.mentions.members.first();
-        if (!member) return message.reply('Invalid usage, please mention the person in this server!');
-        member.addRole(message.guild.roles.find("name", "Mod"));
-        message.reply("That user now has Mod role!");
-        member.guild.channels.find("name", "staff-log").sendMessage(`${member} **has been give Mod role by** ${message.author.tag}`);
-        
-    }
-
-    if(command === `${prefix}trusted`) {
-        if (!message.member.permissions.has('ADMINISTRATOR')) return message.reply("Sorry, you don't have permissions to use this!");
-        const member = message.mentions.members.first();
-        if (!member) return message.reply('Invalid usage, please mention the person in this server!');
-        member.addRole(message.guild.roles.find("name", "Trusted"));
-        message.reply("That user now has Trusted role!");
-        member.guild.channels.find("name", "staff-log").sendMessage(`${member} **has been give Trusted role by** ${message.author.tag}`);        
-    }
-
-    if(command === `${prefix}mute`) {
-        if (!message.member.permissions.has('ADMINISTRATOR')) return message.reply("Sorry, you don't have permissions to use this!");
-        const member = message.mentions.members.first();
-        if (!member) return message.reply('Invalid usage, please mention the person in this server!');
-        member.addRole(message.guild.roles.find("name", "Muted"));
-        member.removeRole(message.guild.roles.find("name", "Friends"));
-        message.reply("The user will now not be able to talk in chat.");
-        member.guild.channels.find("name", "staff-log").sendMessage(`${member} **has been muted by** ${message.author.tag}`);
-    }
-
-    if(command === `${prefix}unmute`) {
-        if (!message.member.permissions.has('ADMINISTRATOR')) return message.reply("Sorry, you don't have permissions to use this!");
-        const member = message.mentions.members.first();
-        if (!member) return message.reply('Invalid usage, please mention the person in this server!');
-        member.removeRole(message.guild.roles.find("name", "Muted"));
-        member.addRole(message.guild.roles.find("name", "Friends"));
-        message.reply("The user will now be able to talk in chat.");
-        member.guild.channels.find("name", "staff-log").sendMessage(`${member} **has been unmuted by** ${message.author.tag}`);
-    }
 });
 
-bot.on("ready", () => bot.user.setGame("in Nemesis Gaming"));
+bot.on("ready", () => bot.user.setGame("burn in hell!"));
 
-bot.login("Mzk1ODA2NDAyNTE1MzA0NDUw.DSYO9Q.0fwlo5bqVjH1dR8nRe9GfkhVKAI");
+bot.login("NDMxMjgxNzMwNTQxMDYwMTA3.DaceAg.qknwuDPH1wFxP3qv04cf36UO6Zk");
